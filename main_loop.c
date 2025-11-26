21029b0424ee7c16571c31b929c0f197e7e443b4
David Ponte <dponte@f1r2s18.codam.nl>
2025-11-26 15:18:26 +0100

    Revert "refactor: improve resource management and error handling in main loop"
    This reverts commit e194dd1e93cbbafec57e1db8c7d8118ad5da7c62.

--- main_loop.c
+++ main_loop.c
@@ -11,28 +11,20 @@
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-static void	safe_close(int fd)
-{
-	if (fd >= 0)
-	{
-		close(fd);
-		fd = -1;
-	}
-}
+#include <unistd.h>
 
 int	first_child(int pipefd1[2], char *infile)
 {
 	int	infile_fd;
 
-safe_close(pipefd1[0]);
+	close(pipefd1[0]);
 	infile_fd = open(infile, O_RDONLY);
 	if (infile_fd  == -1)
-		return (perror("error opening infile "),safe_close(pipefd1[1]), -1);
+		return (perror("error opening infile "), close(pipefd1[1]), -1);
 	dup2(infile_fd, STDIN_FILENO);
 	dup2(pipefd1[1], STDOUT_FILENO);
-safe_close(pipefd1[1]);
-safe_close(infile_fd);
+	close(pipefd1[1]);
+	close(infile_fd);
 	return (0);
 }
 
@@ -40,23 +32,23 @@ int	middle_children(int pipefd1[2], int pipefd2[2], int i)
 {
 	if (i % 2 != 0)
 	{
-	safe_close(pipefd1[1]);
-	safe_close(pipefd2[0]);
+		close(pipefd1[1]);
+		close(pipefd2[0]);
 		if (dup2(pipefd1[0], STDIN_FILENO) == -1)
 			perror("dup2 error");
 		if (dup2(pipefd2[1], STDOUT_FILENO) == -1)
 			perror("dup2 error");
-	safe_close(pipefd1[0]);
-	safe_close(pipefd2[1]);
+		close(pipefd1[0]);
+		close(pipefd2[1]);
 	}
 	if (i % 2 == 0)
 	{
-	safe_close(pipefd2[1]);
-	safe_close(pipefd1[0]);
+		close(pipefd2[1]);
+		close(pipefd1[0]);
 		dup2(pipefd2[0], STDIN_FILENO);
 		dup2(pipefd1[1], STDOUT_FILENO);
-	safe_close(pipefd2[0]);
-	safe_close(pipefd1[1]);
+		close(pipefd2[0]);
+		close(pipefd1[1]);
 	}
 	return (0);
 }
@@ -65,8 +57,8 @@ int	last_child(int pipefd1[2], int pipefd2[2], char *outfile, int i)
 {
 	int		outfile_fd;
 
-safe_close(pipefd1[1]);
-safe_close(pipefd2[1]);
+	close(pipefd1[1]);
+	close(pipefd2[1]);
 	if (i % 2 == 0)
 	{
 		if (dup2(pipefd2[0], STDIN_FILENO) == -1)
@@ -81,52 +73,31 @@ safe_close(pipefd2[1]);
 	if (outfile_fd == -1)
 		return (perror("outfile_fd:"), -1);
 	dup2(outfile_fd, STDOUT_FILENO);
-safe_close(pipefd1[0]);
-safe_close(pipefd2[0]);
-safe_close(outfile_fd);
+	close(pipefd1[0]);
+	close(pipefd2[0]);
+	close(outfile_fd);
 	return (0);
 }
 
 void	back_to_parent(int i, int pipefd1[2], int pipefd2[2])
 {
 	if (i % 2 == 0)
-		safe_close(pipefd1[1]);
+		close(pipefd1[1]);
 	if (i % 2 != 0)
-		safe_close(pipefd2[1]);
+		close(pipefd2[1]);
 }
 
-void	wait_for_children_and_close_open_pipes(int i,
-				int pipefd1[2], int pipefd2[2])
+void	wait_children_close_open_pipes(int i, int pipefd1[2], int pipefd2[2])
 {
 	while (i >= 0)
 	{
 		wait(0);
 		i--;
 	}
-	safe_close(pipefd1[0]);
-	safe_close(pipefd2[0]);
-}
-
-static int	create_pipes(int pipefd1[2], int pipefd2[2])
-{
-	pipefd1[0] = -1;
-	pipefd2[0] = -1;
-	pipefd1[1] = -1;
-	pipefd2[1] = -1;
-
-	if (pipe(pipefd1) == -1)
-	{
-		perror("main: pipe creation");
-		return (-1);
-	}
-	if (pipe(pipefd2) == -1)
-	{
-		safe_close(pipefd1[0]);
-		safe_close(pipefd1[1]);
-		perror("main: pipe creation");
-		return (-1);
-	}
-	return (0);
+	if (pipefd1[0] != -1)
+		close(pipefd1[0]);
+	if (pipefd2[0] != -1)
+		close(pipefd2[0]);
 }
 
 int	main_loop(int argc, char *argv[], char *env[], char *paths[])
@@ -139,8 +110,8 @@ int	main_loop(int argc, char *argv[], char *env[], char *paths[])
 
 	i = 0;
 	maxchildren = argc - 4;
-	if (create_pipes(pipefd1, pipefd2) == -1)
-		return (-1);
+	if ((pipe(pipefd1) == -1) || (pipe(pipefd2) == -1))
+		return (perror("pipe creation"), -1);
 	while (i <= maxchildren)
 	{
 		pid = fork();
@@ -150,8 +121,8 @@ int	main_loop(int argc, char *argv[], char *env[], char *paths[])
 		{
 			first_child(pipefd1, argv[1]);
 			execute_cmd(paths, argv[2], env);
-			safe_close(pipefd2[0]);
-			safe_close(pipefd2[1]);
+			close(pipefd2[0]);
+			close(pipefd2[1]);
 			return (perror("execve"), -1);
 		}
 		if (pid == 0 && i == maxchildren)
@@ -170,7 +141,6 @@ int	main_loop(int argc, char *argv[], char *env[], char *paths[])
 			back_to_parent(i, pipefd1, pipefd2);
 		i++;
 	}
-	wait_for_children_and_close_open_pipes(i, pipefd1, pipefd2);
+	wait_children_close_open_pipes(i, pipefd1, pipefd2);
 	return (0);
 }
-
